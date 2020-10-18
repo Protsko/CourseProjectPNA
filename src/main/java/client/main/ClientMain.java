6bbaa8b5fa3b0b719c719fd84a102f4a69060c9c
Protsko <62498609+Protsko@users.noreply.github.com>
2020-10-18 18:53:21 +0300

    Lost commit
    It should be before User entity creation. Creation of hole project structure

--- src/main/java/client/main/ClientMain.java
+++ src/main/java/client/main/ClientMain.java
@@ -1,7 +1,5 @@
 package client.main;
 
-import
-
 public class ClientMain {
-    public static void main(String[] args) {new LoginFrame(); }
+    public static void main(String[] args) {/*new LoginFrame();*/ }
 }
