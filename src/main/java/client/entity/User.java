c463e9567946394379b0aecd388384558d85ce13
Protsko <62498609+Protsko@users.noreply.github.com>
2020-10-18 18:52:14 +0300

    User entity
    Code for such entity like User was created

--- src/main/java/client/entity/User.java
+++ src/main/java/client/entity/User.java
@@ -1,4 +1,77 @@
 package client.entity;
 
+import java.util.Objects;
+
 public class User {
+
+    private Long id;
+    private String login;
+    private String password;
+    private UserRole userRole;
+
+    public User(Long id, String login, String password, UserRole userRole) {
+        this.id = id;
+        this.login = login;
+        this.password = password;
+        this.userRole = userRole;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public UserRole getUserRole() {
+        return userRole;
+    }
+
+    public void setUserRole(UserRole userRole) {
+        this.userRole = userRole;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id) &&
+                Objects.equals(login, user.login) &&
+                Objects.equals(password, user.password) &&
+                userRole == user.userRole;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, login, password, userRole);
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("User{");
+        sb.append("id=").append(id);
+        sb.append(", login='").append(login).append('\'');
+        sb.append(", password='").append(password).append('\'');
+        sb.append(", userRole=").append(userRole);
+        sb.append('}');
+        return sb.toString();
+    }
 }
