a52069265fd1213ac9cda4e90de37fd78d61c67f
Protsko <62498609+Protsko@users.noreply.github.com>
2020-10-20 22:45:11 +0300

    ProductFrame
    Added components for future product form(start page)

--- src/main/java/client/frame/ProductFrame.java
+++ src/main/java/client/frame/ProductFrame.java
@@ -1,4 +1,23 @@
 package client.frame;
 
-public class ProductFrame {
+import client.main.SocketJFrame;
+
+import javax.swing.*;
+
+public class ProductFrame extends SocketJFrame {
+
+    private final OrderFrame orderFrame = new OrderFrame();
+    private final LoginFrame loginFrame = new LoginFrame();
+
+    private DefaultListModel model = new DefaultListModel();
+    private JPanel mainPanel;
+    private JList productList;
+    private JLabel userNameLabel = new JLabel();
+    private JButton loginButton = new JButton("Вход");
+    private JButton logoutButton = new JButton("Выход");
+    private JButton infoButton;
+    private JButton toOrderButton;
+    private JButton myOrdersButton;
+    private JButton deleteButton;
+
 }
--- src/main/java/client/main/ClientMain.java
+++ src/main/java/client/main/ClientMain.java
@@ -1,5 +1,7 @@
 package client.main;
 
+import client.frame.ProductFrame;
+
 public class ClientMain {
-    public static void main(String[] args) {/*new LoginFrame();*/ }
+    public static void main(String[] args) {new ProductFrame(); }
 }
