4d7e595435473fdaef76e99b80a33cba0549e0c2
Protsko <62498609+Protsko@users.noreply.github.com>
2020-10-25 16:29:04 +0300

    Update AppContext.java
    Created code for appcontext.java

--- src/main/java/server/app/AppContext.java
+++ src/main/java/server/app/AppContext.java
@@ -1,6 +1,54 @@
 package server.app;
 
+import server.dao.OrderDao;
+import server.dao.ProductDao;
+import server.dao.UserDao;
+import server.dao.mysql.MySQLOrderDao;
+import server.dao.mysql.MySQLProductDao;
+import server.dao.mysql.MySQLUserDao;
+import server.service.OrderService;
+import server.service.ProductService;
+import server.service.UserService;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class AppContext {
 
+    private static AppContext instance;
 
+    private final Map<Class<?>, Object> beans;
+
+    private AppContext() {
+        UserDao userDao = new MySQLUserDao();
+        ProductDao productDao = new MySQLProductDao();
+        OrderDao orderDao = new MySQLOrderDao();
+        UserService userService = new UserService(userDao);
+        ProductService productService = new ProductService(productDao);
+        OrderService orderService = new OrderService(orderDao);
+
+        beans = new HashMap<>();
+        addBean(UserService.class, userService);
+        addBean(ProductService.class, productService);
+        addBean(OrderService.class, orderService);
+    }
+
+    public void addBean(Class<?> clazz, Object obj) {
+        if (obj.getClass() == clazz) {
+            beans.put(clazz, obj);
+        } else {
+            throw new IllegalStateException();
+        }
+    }
+
+    public Object getBean(Class<?> clazz) {
+        return beans.get(clazz);
+    }
+
+    public static AppContext getInstance() {
+        if (instance == null) {
+            instance = new AppContext();
+        }
+        return instance;
+    }
 }
