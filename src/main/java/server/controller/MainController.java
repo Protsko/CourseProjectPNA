78632b12b027c0db98a2a391bacb9c8984a85cbc
Protsko <62498609+Protsko@users.noreply.github.com>
2020-10-25 16:17:06 +0300

    System commit
    Lost file

--- src/main/java/server/controller/MainController.java
+++ src/main/java/server/controller/MainController.java
@@ -1,7 +1,12 @@
 package server.controller;
 
+import server.controller.command.Command;
+import server.controller.command.CommandProvider;
+
 import java.io.*;
 import java.net.*;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainController {
     public void processRequest(Socket clientSocket) throws IOException {
@@ -12,5 +17,27 @@ public class MainController {
                 new InputStreamReader(clientSocket.getInputStream())
         );
 
+        String request = bufferedReader.readLine();
+        String[] splitRequest = request.split("&");
+        Map<String, String> params = getRequestParams(splitRequest);
+        String commandName = CommandProvider.getCommandName(splitRequest);
+        Command command = CommandProvider.defineCommand(commandName);
+        String response = command.execute(params);
+
+        bufferedWriter.write(response + "\n");
+        bufferedWriter.flush();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+        clientSocket.close();
+    }
+
+    private Map<String, String> getRequestParams(String[] splitRequest) {
+        Map<String, String> params = new HashMap<>();
+        for (String sr : splitRequest) {
+            String[] s = sr.split("=");
+            params.put(s[0], s[1]);
+        }
+        return params;
     }
 }
